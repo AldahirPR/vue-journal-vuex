54714f128dd584889ba20b96db7bed4ed14c714c
Aldahir Pulido <aldahipr98@gmail.com>
2023-04-22 17:48:21 -0600

    Primer commit

---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,23 @@
+export default () => ({
+    isLoading: true,
+    entries: [
+        {
+            id: new Date().getTime(), //616546548
+            date: new Date().toDateString(), //sat 23, july
+            text: 'Reprehenderit qui eiusmod magna ea sint sit sunt Lorem enim pariatur fugiat exercitation exercitation velit. Excepteur commodo sunt dolor nisi et Lorem ullamco nostrud. Est labore nostrud dolore pariatur nulla eu et. Pariatur ea culpa commodo anim nostrud ex officia in anim occaecat sit quis. Tempor exercitation amet duis esse proident nisi do. Amet commodo labore magna anim voluptate consectetur officia cillum reprehenderit qui magna incididunt labore culpa.',
+            picture: null,
+        },
+        {
+            id: new Date().getTime() + 1000, //616546548
+            date: new Date().toDateString(), //sat 23, july
+            text: 'Sit consequat incididunt dolore cillum nostrud occaecat. Velit veniam dolor consectetur exercitation dolor mollit ad. Amet cupidatat amet veniam duis anim dolor incididunt est. Enim excepteur incididunt ut velit ex incididunt duis occaecat nostrud laborum nisi fugiat.',
+            picture: null,
+        },
+        {
+            id: new Date().getTime() + 2000, //616546548
+            date: new Date().toDateString(), //sat 23, july
+            text: 'Nisi reprehenderit ea anim commodo. Aliquip fugiat Lorem elit deserunt ea aute elit id nisi anim sunt. Consequat magna ut est ut sit irure in non amet do. Do excepteur aute consectetur ad irure enim eiusmod ex cupidatat tempor est. Irure proident ex qui irure dolore in fugiat cupidatat aliqua commodo magna.',
+            picture: null,
+        }
+    ]
+})
